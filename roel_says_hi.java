de7098212c70b6cff5dc4c639a962872be4adc68
Harshpreetkaur98 <138805558+Harshpreetkaur98@users.noreply.github.com>
2024-01-23 16:36:15 +0000

    harshpreet_roel_says_hi.java

--- roel_says_hi.java
+++ roel_says_hi.java
@@ -1,5 +1,6 @@
 public class roel_says_hi {
     public static void main(String[] args) {
         System.out.println("Roel says hi!");
+        System.out.printlin("Harshpreet says Fuck you Java!");
     }
 }
